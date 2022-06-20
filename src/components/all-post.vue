4e9556f72ac2677cf28c38f2f5ec593c5ff7e4a2
Hassan Shahriar <41904536+Hassan-Shahriar-1@users.noreply.github.com>
2022-06-21 00:36:12 +0600

    final

--- src/components/all-post.vue
+++ src/components/all-post.vue
@@ -4,10 +4,10 @@
 
         <div class="container">
   
-        <h2 class="text-center">Post List with Badges</h2>
+        <h2 class="text-center">Post List </h2>
         
-        
-        <div class="list-group">
+        <div class="row" v-if="postlist!=''">
+        <div class="list-group col-10">
           <div href="#" class="list-group-item border rounded py-4 bg-second my-2" v-for="(plist,i) in postlist" :key="i">
                 <div class="row">
                 <h6 class="text-center col-9 float-left">{{plist.title}}</h6>
@@ -16,6 +16,21 @@
             <p>{{plist.description}}</p>
             </div>
           
+        </div>
+        <div class="col-2">
+            <div class="card">
+                <h6 class="text-left text-info">category list</h6>
+            <div class="card  text-center"  v-for="(category,i) in cat" :key="i" @click="load_content(category.name)">{{category.name}}</div>
+            </div>
+        </div>
+        </div>
+        <div v-else class="row"><h5 class="text-danger col-10">sorry no data found</h5>
+        <div class="col-2">
+            <div class="card">
+                <h6 class="text-left text-info">category list</h6>
+            <div class="card  text-center"  v-for="(category,i) in cat" :key="i" @click="load_content(category.name)">{{category.name}}</div>
+            </div>
+        </div>
         </div>
       
         
@@ -37,6 +52,25 @@ export default{
     data() {
         return {
             allpost:'',
+            cat:'',
+        }
+    },
+    beforeCreate() {
+        this.axios.get('/category/list').then((res)=>{
+            if(res.sts=='cat-data'){
+                this.cat=res.data
+            }
+        })
+    },
+    methods: {
+        load_content(name){
+            this.axios.get('/post/'+name).then((res)=>{
+                if(res.sts=='found'){
+                    this.$props.postlist    =res.data
+                }else{
+                    this.$props.postlist=''
+                }
+            })
         }
     },
     
